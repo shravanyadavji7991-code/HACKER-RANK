7ebc3d142aacd28c487b3da5c503480ea5cc8398
shravanyadavji7991-code <shravanyadavji7991@gmail.com>
2025-12-12 02:02:35 +0530

    hacker-rank day2

---
+++ lowercase to UPPERCASE.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char a;
+     scanf("%c ",&a);
+    char b = a-32;
+    scanf("%c",&b);
+    printf("The uppercase of %c is %c",a,b);
+    
+    return 0;
+}
