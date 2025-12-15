86f922cef84a43b4966a8721dcfa9952b9d16ac9
shravanyadavji7991-code <shravanyadavji7991@gmail.com>
2025-12-15 22:34:35 +0530

    hacker rank day 3

---
+++ Sum of Two Numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+ int a ;
+    int b;
+    scanf("%d %d ",&a, &b);
+    printf("The sum of %d and %d is %d.",a, b,a+b );
+    return 0;
+}
